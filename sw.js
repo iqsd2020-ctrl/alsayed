31d4a62fb2920bb7524bf8de1f2bb713e0f8b996
iqsd2020-ctrl <iq.sd.2020@gmail.com>
2026-01-29 16:41:58 +0300

    Update cache version from v2 to v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - منهج النور */
-const CACHE_VERSION = 'v2';;
+const CACHE_VERSION = 'v3';;
 const CACHE_NAME = `manhaj-alnoor-${CACHE_VERSION}`;
 
 // ملفات أساسية (عدّل/زد القائمة إذا رغبت لاحقاً)
